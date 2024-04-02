a197495a36259d7faadc37bb78e09563ea23c4c9
xilconic <xilconic@gmail.com>
2024-04-02 17:19:30 +0200

    NukeBuild script to creating releases

---
+++ NukeBuild/Build.cs
@@ -0,0 +1,257 @@
+using System;
+using System.Linq;
+using System.IO;
+using System.IO.Compression;
+using Nuke.Common;
+using Nuke.Common.CI;
+using Nuke.Common.Execution;
+using Nuke.Common.IO;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tooling;
+using Nuke.Common.Tools.Unity;
+using Nuke.Common.Utilities.Collections;
+using static Nuke.Common.EnvironmentInfo;
+using static Nuke.Common.IO.FileSystemTasks;
+using static Nuke.Common.IO.PathConstruction;
+using System.Text.RegularExpressions;
+using Serilog;
+using Nuke.Common.Git;
+using Nuke.Common.Tools.Git;
+using Nuke.Common.Tools.GitHub;
+using Octokit;
+using _build;
+using System.Collections.Generic;
+using System.Net.Mime;
+
+partial class Build : NukeBuild
+{
+    private const string AppName = "ChrisTutorialsPlatformerCrashCourse";
+
+    private static readonly AbsolutePath _unityGameSrcDirectory = RootDirectory / "ChrisTutorialsPlatformerCrashCourse";
+    private static readonly AbsolutePath _buildsDirectory = RootDirectory / "Builds";
+    private static readonly AbsolutePath _winx64BuildDirectory = _buildsDirectory / "winx64";
+    private static readonly AbsolutePath _winx86BuildDirectory = _buildsDirectory / "winx86";
+
+    [GitRepository] readonly GitRepository Repository;
+
+    [Parameter("The version of the build taking place; Needs to be formatted in x.y.z, optionally post-fixes with -alpha or -beta")] 
+    readonly string Version;
+
+    [Parameter("The 'Personal Access Token' used by the Build scripts for creating releases"), Secret]
+    readonly string GitHubPersonalAccessToken;
+
+    private readonly Dictionary<ReleaseTypes, AbsolutePath> _targetAppFileLocations = new();
+    private string _releaseVersion;
+
+    public static int Main() => Execute<Build>();
+
+    Target SetVersion => _ => _
+        .Requires(() => Version)
+        .Unlisted()
+        .Executes(() =>
+        {
+            Assert.True(GetValidVersionRegex().IsMatch(Version), "The 'Version' Parameter needs to be in the 'SemVer' fomat of x.y.z, with optionally -alpha or -beta postfix.");
+
+            var projectSettingsFile = _unityGameSrcDirectory / "ProjectSettings" / "ProjectSettings.asset";
+            // Match to `  bundleVersion: x.y.z-alpha` or `  bundleVersion: x.y.z-beta` or `  bundleVersion: x.y.z`:
+            Regex projectSettingsVersionRegex = GetProjectSettingsVersionRegex();
+            UpdateVersionTextInFileUsingRegex(Version!, projectSettingsFile, projectSettingsVersionRegex, "bundleVersion: <version>");
+        });
+
+    Target BuildAppAsWin64bit => _ => _
+        .Executes(() =>
+        {
+            var targetAppFileLocation = _winx64BuildDirectory / $"{AppName}.exe";
+            BuildWin64BitInTargetDirectory(targetAppFileLocation);
+        });
+
+    Target BuildAppAsWin32bit => _ => _
+        .Executes(() =>
+        {
+            var targetAppFileLocation = _winx86BuildDirectory / $"{AppName}.exe";
+            BuildWin32BitInTargetDirectory(targetAppFileLocation);
+        });
+
+    Target BuildWin64BitReleaseBinaries => _ => _
+        .DependsOn(SetVersion)
+        .Unlisted()
+        .Executes(() =>
+        {
+            _releaseVersion = $"{AppName}_winx64_{Version}";
+            _targetAppFileLocations[ReleaseTypes.Windows64Bit] = _winx64BuildDirectory / _releaseVersion / $"{AppName}.exe";
+            BuildWin64BitInTargetDirectory(_targetAppFileLocations[ReleaseTypes.Windows64Bit], true);
+            
+        });
+
+    Target BuildWin32BitReleaseBinaries => _ => _
+        .DependsOn(SetVersion)
+        .Unlisted()
+        .Executes(() =>
+        {
+            _releaseVersion = $"{AppName}_winx86_{Version}";
+            _targetAppFileLocations[ReleaseTypes.Windows32Bit] = _winx86BuildDirectory / _releaseVersion / $"{AppName}.exe";
+            BuildWin32BitInTargetDirectory(_targetAppFileLocations[ReleaseTypes.Windows32Bit], true);
+
+        });
+
+    Target BuildWin64BitReleaseZipFile => _ => _
+        .DependsOn(BuildWin64BitReleaseBinaries)
+        .Executes(() =>
+        {
+            ZipRelease(ReleaseTypes.Windows64Bit);
+        });
+
+    Target BuildWin32BitReleaseZipFile => _ => _
+        .DependsOn(BuildWin32BitReleaseBinaries)
+        .Executes(() =>
+        {
+            ZipRelease(ReleaseTypes.Windows32Bit);
+        });
+
+    Target CreateReleaseCommitAndPush => _ => _
+        .DependsOn(SetVersion)
+        .Requires(() => Repository.IsOnMainBranch()) // This project uses Trunk Based Development
+        .Executes(() =>
+        {
+            // Stage all side-effects of SetVersion Target:
+            GitTasks.Git("add .");
+
+            // Create release commit:
+            GitTasks.Git($"commit -m \"Build {Version}\"");
+
+            // Tag release commit:
+            GitTasks.Git($"tag -a {Version} -m \"Tag build {Version}\"");
+
+            // Push to origin:
+            GitTasks.Git("push origin --tags main:main");
+        });
+
+    Target CreateReleaseOnGithub => _ => _
+        .Requires(() => Version)
+        .Requires(() => GitHubPersonalAccessToken)
+        .DependsOn(CreateReleaseCommitAndPush)
+        .DependsOn(BuildWin64BitReleaseZipFile)
+        .DependsOn(BuildWin32BitReleaseZipFile)
+        .Executes(async () =>
+        {
+            string branchName = await GitHubTasks.GetDefaultBranch(Repository);
+            var productInformation = new ProductHeaderValue($"{AppName}-NukeBuildScript");
+            GitHubTasks.GitHubClient = new GitHubClient(productInformation)
+            {
+                Credentials = new Credentials(GitHubPersonalAccessToken)
+            };
+
+            bool isPrerelease = Version.EndsWith("alpha") || Version.EndsWith("beta");
+            string namePrefix = isPrerelease ? "Pre-release ": string.Empty;
+            var releaseData = new NewRelease(Version)
+            {
+                Prerelease = isPrerelease,
+                Body = "TODO",
+                Draft = true,
+                Name = $"{namePrefix}{Version}"
+            };
+            try
+            {
+                Release release = await GitHubTasks.GitHubClient.Repository.Release
+                    .Create(Repository.GetGitHubOwner(), Repository.GetGitHubName(), releaseData);
+
+                foreach (ReleaseTypes releaseType in _targetAppFileLocations.Keys)
+                {
+                    AbsolutePath releaseArtifact = GetReleaseZipFilePath(releaseType);
+                    var upload = new ReleaseAssetUpload
+                    {
+                        ContentType = MediaTypeNames.Application.Zip,
+                        FileName = Path.GetFileName(releaseArtifact),
+                        RawData = File.OpenRead(releaseArtifact)
+                    };
+                    await GitHubTasks.GitHubClient.Repository.Release
+                                    .UploadAsset(release, upload);
+                }
+            }
+            catch (ApiException e)
+            {
+                Log.Debug("{@ExceptionData}", e);
+                Log.Error(e, "Failure during release creation towards Github");
+            }
+            
+        });
+
+    private static void BuildWin64BitInTargetDirectory(AbsolutePath targetAppFileLocation, bool isReleaseBuild = false)
+    {
+        BuildInTargetDirectory(
+            unitySettings => _build.UnitySettingsExtensions.SetWindows64BitBuildTarget(unitySettings, targetAppFileLocation),
+            isReleaseBuild
+        );
+    }
+
+    private static void BuildWin32BitInTargetDirectory(AbsolutePath targetAppFileLocation, bool isReleaseBuild = false)
+    {
+        BuildInTargetDirectory(
+            unitySettings => _build.UnitySettingsExtensions.SetWindows32BitBuildTarget(unitySettings, targetAppFileLocation), 
+            isReleaseBuild
+        );
+    }
+
+    private static void BuildInTargetDirectory(
+        Func<UnitySettings, UnitySettings> setBuildTarget,
+        bool isReleaseBuild = false)
+    {
+        UnityTasks.Unity(unitySettings =>
+        {
+            UnitySettings settings = unitySettings
+                .EnableQuit()
+                .EnableBatchMode()
+                .SetProjectPath(_unityGameSrcDirectory)
+                .SetLogFile(RootDirectory / "NukeBuild" / "unity.log");
+            settings = setBuildTarget(settings);
+            if (isReleaseBuild)
+            {
+                settings.AddCustomArguments("-releaseCodeOptimization");
+            }
+            return settings;
+        });
+    }
+
+    private static void UpdateVersionTextInFileUsingRegex(string versionText, AbsolutePath fileToBeUpdated, Regex regex, string expectedPattern)
+    {
+        Assert.FileExists(fileToBeUpdated);
+        string[] fileLines = fileToBeUpdated.ReadAllLines();
+
+        bool updatedVersion = false;
+        for (int i = 0; i < fileLines.Length; i++)
+        {
+            var line = fileLines[i];
+            var match = regex.Match(line);
+            if (match.Success)
+            {
+                Group currentVersionCaptureGroup = match.Groups["currentVersion"];
+                fileLines[i] = line.Replace(currentVersionCaptureGroup.Value, versionText);
+                updatedVersion = true;
+                break;
+            }
+        }
+        if (!updatedVersion)
+        {
+            Assert.Fail($"\"{expectedPattern}\" pattern missing from '{fileToBeUpdated}'.");
+        }
+        fileToBeUpdated.WriteAllLines(fileLines);
+        Log.Debug("Successfully updated {File} to {NewVersion}", fileToBeUpdated.ToString(), versionText);
+    }
+
+    private void ZipRelease(ReleaseTypes release)
+    {
+        AbsolutePath archiveFile = GetReleaseZipFilePath(release);
+        AbsolutePath releaseDirectory = _targetAppFileLocations[release].Parent;
+        releaseDirectory.ZipTo(
+            archiveFile,
+            compressionLevel: CompressionLevel.SmallestSize
+        );
+    }
+
+    private AbsolutePath GetReleaseZipFilePath(ReleaseTypes win64BitRelease) => _targetAppFileLocations[win64BitRelease].Parent + ".zip";
+
+    [GeneratedRegex("^\\s+bundleVersion:\\s+(?<currentVersion>\\d+\\.\\d+\\.\\d+(-(alpha|beta))?)$")]
+    private static partial Regex GetProjectSettingsVersionRegex();
+    [GeneratedRegex("^\\d+\\.\\d+\\.\\d+(-(alpha|beta))?$")]
+    private static partial Regex GetValidVersionRegex();
+}
--- README.md
+++ README.md
@@ -52,3 +52,90 @@ Documentation: https://docs.unity3d.com/Packages/com.unity.cinemachine@2.9/manua
 ### Input System
 Uses version 1.7.0 of the `Input System` package by Unity Technologies Inc.
 Documentation: https://docs.unity3d.com/Packages/com.unity.inputsystem@1.7/manual/index.html
+
+## Building and Running locally
+
+### Requirements
+- [Unity](https://unity.com/releases/editor/archive) v2022.3.21f1
+- [Visual Studio](https://visualstudio.microsoft.com/vs/community/) v17.9.2
+	- With "Game development with Unity" extension
+- [Python](https://www.python.org/downloads/) v3.12.2
+- [Nuke](https://nuke.build/docs/introduction/) v8.0.0 (Installed from commandline: `dotnet tool install Nuke.GlobalTool --global`)
+
+#### Configuring Github release capabilities
+In the `.nuke/parameters.secrets.json` the `GitHubPersonalAccessToken` parameter is defined.
+https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens#creating-a-fine-grained-personal-access-token describes how to define one for the Build Script.
+The token requires the following grants:
+- Read+Write to contents (needed for creating Releases and uploading files to a release)
+
+To generate the `.nuke/parameters.secrets.json` file:
+1. Open commandline in repository root.
+2. Run `nuke :secrets secrets`
+3. Provide a password (Save this to something like a Password Safe, as you'll require to provide it for build targets that depend on secrets)
+4. Navigate the menu to `GitHubPersonalAccessToken` and provide a value.
+5. Navigate the menu to `<Save and Exit>`.
+
+### Building a Github Release (build script)
+1. Open commandline in repository root.
+2. Run `nuke --target CreateReleaseOnGithub --profile secrets --Version x.y.z`
+	- The `Version` parameter can optionally be post-fixed with -alpha or -beta
+	- Not specifying the `Version` parameter causes the script to ask you interactively in the commandline
+3. Provide the password for the secrets file
+
+### Building a Windows Release (build script)
+
+#### 32bit
+1. Open commandline in repository root.
+2. Run `nuke --target BuildWin32BitReleaseZipFile --Version x.y.z`
+	- The `Version` parameter can optionally be post-fixed with -alpha or -beta
+	- Not specifying the `Version` parameter causes the script to ask you interactively in the commandline
+3. The zip-file can be found in `<repo root>\Builds\winx86`
+#### 64bit 
+1. Open commandline in repository root.
+2. Run `nuke --target BuildWin64BitReleaseZipFile --Version x.y.z`
+	- The `Version` parameter can optionally be post-fixed with -alpha or -beta
+	- Not specifying the `Version` parameter causes the script to ask you interactively in the commandline
+3. The zip-file can be found in `<repo root>\Builds\winx64`
+
+### Creating Release Commit
+1. Open commandline in repository root.
+2. Run `nuke --target CreateReleaseCommitAndPush --Version x.y.z`
+	- The `Version` parameter can optionally be post-fixed with -alpha or -beta
+	- Not specifying the `Version` parameter causes the script to ask you interactively in the commandline
+
+### Windows (build script)
+
+#### 32bit
+1. Open commandline in repository root.
+2. Run `nuke --target BuildAppAsWin32bit`.
+3. Build can be found in `<repo root>\Builds\winx86`.
+
+#### 64bit
+1. Open commandline in repository root.
+2. Run `nuke --target BuildAppAsWin64bit`.
+3. Build can be found in `<repo root>\Builds\winx64`.
+
+### Windows (manual)
+1. From repository root, open `ChrisTutorialsPlatformerCrashCourse` folder.
+2. File > Build Settings
+3. Ensure Platform `Windows, Mac, Linux` is active.
+	- If not, click the `Switch Platform` button in the lower right corner and wait for the switch to finish.
+4. Click the 'Build and Run' button.
+5. When asked, determine the location to build
+	- Recommendation 32 bit: `<repo root>\Builds\winx86` for an `Windows` target platform and `Intel 32-bit` architecture
+	- Recommendation 64 bit: `<repo root>\Builds\winx64` for an `Windows` target platform and `Intel 64-bit` architecture
+6. Wait for the build to complete. The game will start automatically after.
+
+### HTML 5 (manual)
+1. From repository root, open `ChrisTutorialsPlatformerCrashCourse` folder.
+2. File > Build Settings
+3. Ensure Platform `WebGL` is active.
+	- If not, click the `Switch Platform` button in the lower right corner and wait for the switch to finish.
+4. Click the 'Build' button.
+5. When asked, determine the location to build
+	- Recommendation: `<repo root>\Builds\html5`
+6. Wait for the build to complete.
+7. Navigate into the build folder.
+8. Open a command line in that folder.
+9. Run the command: `python -m http.server`
+10. Open a browser. Naviate to `localhost:8000` (assuming 8000 portnumber; Specific port number is mentioned in the command line window). Game is now started.
