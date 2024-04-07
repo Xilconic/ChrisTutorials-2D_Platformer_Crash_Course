d31f6095b12be90dfdc64d1c3f31f5d9b94dbfd9
xilconic <xilconic@gmail.com>
2024-04-07 11:37:13 +0200

    Play sound effect when collecting healing fruit

--- ChrisTutorialsPlatformerCrashCourse/Assets/Scripts/HealthPickup.cs
+++ ChrisTutorialsPlatformerCrashCourse/Assets/Scripts/HealthPickup.cs
@@ -2,13 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class HealthPickup : MonoBehaviour
 {
+    AudioSource _audioSource;
+
     [Tooltip("The amount of health restored")]
     public int HealthRestored = 20;
 
     [Tooltip("The speed, in degrees/s, at which the pickup rotates")]
     public Vector3 SpinRotationSpeed = new Vector3(0, 180, 0);
+
+    private void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
 
     private void Update()
     {
@@ -22,6 +30,7 @@ public class HealthPickup : MonoBehaviour
             bool wasHealed = damagable.Heal(HealthRestored);
             if(wasHealed)
             {
+                AudioSource.PlayClipAtPoint(_audioSource.clip, transform.position, _audioSource.volume);
                 Destroy(gameObject);
             }
         }
