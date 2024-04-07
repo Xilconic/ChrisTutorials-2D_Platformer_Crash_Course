681d4aa36428bcc88d1a47652a8240029367e9b1
xilconic <xilconic@gmail.com>
2024-04-07 10:44:45 +0200

    Allow exiting the game when pressing Escape

--- ChrisTutorialsPlatformerCrashCourse/Assets/Scripts/UIManager.cs
+++ ChrisTutorialsPlatformerCrashCourse/Assets/Scripts/UIManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class UIManager : MonoBehaviour
 {
@@ -43,5 +44,21 @@ public class UIManager : MonoBehaviour
         TMP_Text text = Instantiate(healthTextPrefab, spawnPosition, Quaternion.identity, _gameCanvas.transform)
             .GetComponent<TMP_Text>();
         text.text = healthRestored.ToString();
+    }
+
+    public void OnExitGame(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+#if (Unity_Editor || DEVELOPMENT_BUILD)
+            Debug.Log($"{this.name} : {nameof(UIManager)} : {nameof(OnExitGame)}");
+#endif
+
+#if (UNITY_EDITOR)
+            UnityEditor.EditorApplication.isPlaying = false;
+#elif(UNITY_STANDALONE)
+            Application.Quit();
+#endif
+        }
     }
 }
